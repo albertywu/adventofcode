fd0ce0bf19f419f56c9576e2e134cd8e3c535e3e
Albert Wu <albertywu@gmail.com>
2022-12-06 14:28:28 -0800

    remove unnecessary check

--- 2022/python/6.py
+++ 2022/python/6.py
@@ -1,8 +1,6 @@
 with open('6.in') as f:
     letters = list(f.read().strip())
     for idx, c in enumerate(letters):
-        if idx == len(letters) - 3:
-            break
         if len(set(letters[idx: idx + 4])) == 4:
             print(idx + 4)
             break

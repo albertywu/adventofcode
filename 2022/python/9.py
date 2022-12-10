fef0429c9ec8bbaa8e543c6364dde99439b00bbd
Albert Wu <albertywu@gmail.com>
2022-12-09 20:07:11 -0800

    2022 - day 9b

--- 2022/python/9.py
+++ 2022/python/9.py
@@ -2,10 +2,16 @@ with open("9.in") as f:
     inp = f.read().strip()
 
 
-def update(head, tail, dx, dy):
-    head = (head[0] + dx, head[1] + dy)
-    tail = update_tail(head, tail)
-    return head, tail
+# a rope consists of a list of tuples.
+# the first tuple is the head of the rope, and the last tuple is the tail of the rope
+def update(rope, dx, dy):
+    # update head
+    rope[0] = (rope[0][0] + dx, rope[0][1] + dy)
+    # update everything after head
+    for idx in range(1, len(rope)):
+        prev = rope[idx - 1]
+        rope[idx] = update_tail(prev, rope[idx])
+    return rope
 
 
 def update_tail(head, tail):
@@ -25,8 +31,10 @@ def update_tail(head, tail):
 
 
 def solve():
-    head = (0, 0)
-    tail = (0, 0)
+    rope_length = 10
+    rope = []
+    for i in range(0, rope_length):
+        rope.append((0, 0))
     tail_visits = set()
 
     for line in inp.split("\n"):
@@ -42,11 +50,9 @@ def solve():
         elif d == "D":
             dx, dy = 0, -1
         for i in range(cnt):
-            head, tail = update(head, tail, dx, dy)
-            print(head, tail)
-            tail_visits.add(tail)
+            rope = update(rope, dx, dy)
+            tail_visits.add(rope[len(rope) - 1])
 
-    print(tail_visits)
     print(len(tail_visits))
 
 solve()
